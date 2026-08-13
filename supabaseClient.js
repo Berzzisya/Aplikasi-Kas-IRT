e541690e0f28e03491a66a92eca08faf3fca4181
Berzzisya <berliana.30harahap@gmail.com>
2026-08-13 13:48:03 +0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Ganti nilai di bawah dengan URL dan Anon Key dari Dashboard Supabase Anda
-const SUPABASE_URL = 'https://XXXXXXXXXXXXXX.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+const SUPABASE_URL = 'https://gzorulptwhxsdlmlvdpq.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_x4H-WbmNgxhwqsLpANbF9g_itNl2NM5';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
