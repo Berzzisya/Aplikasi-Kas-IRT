eb5b37fb1baca5e846f55155b5de8d7c3a060e2b
Berzzisya <berliana.30harahap@gmail.com>
2026-08-13 19:00:01 +0700

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Code.JSX'
+import App from './Code.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
